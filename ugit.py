a3fd754ae41216732e1417b0254189448dd76c15
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2022-12-14 10:54:10 -0600

    Update ugit.py

--- ugit.py
+++ ugit.py
@@ -6,8 +6,13 @@ import urequests
 import urepl
 import json
 
-user = 'turfptax'
-repository = 'ugit'
+# CHANGE TO YOUR REPOSITORY INFO
+# Also check out my friends amazing work
+user = 'hwiguna'
+repository = 'HariFun_208_Polymorph'
+
+# Static URLS
+# GitHub uses main instead of master for python repository trees
 giturl = 'https://github.com/{user}/{repository}'
 call_trees_url = f'https://api.github.com/repos/{user}/{repository}/git/trees/main?recursive=1'
 raw = f'https://raw.githubusercontent.com/{user}/{repository}/master/'
@@ -17,7 +22,6 @@ wlan = urepl.wificonnect()
 def pull(f_path,giturl=giturl):
   #files = os.listdir()
   r = urequests.get(giturl)
-  first_line = r.content.decode('utf-8').split('\n')[0]
   new_file = open(f_path, 'w')
   new_file.write(r.content.decode('utf-8'))
   new_file.close()
@@ -36,6 +40,10 @@ def pull_all_files(tree=call_trees_url,raw = raw):
       except:
         print('failed to make directory may already exist')
     elif i['path'] != '.gitignore':
+      try:
+        os.remove(i['path'])
+      except:
+        print('failed to delete old file')
       pull(i['path'],raw + i['path'])
       try:
         check.append(i['path'])
