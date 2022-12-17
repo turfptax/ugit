cb83a4bddc439296feeed92c5be4565b3018f85f
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2022-12-16 21:19:35 -0600

    Update ugit_config.py

--- ugit_config.py
+++ ugit_config.py
@@ -3,4 +3,5 @@
 user = 'turfptax'
 repository = 'ugit_test'
 
+# Don't remove ugit.py from the ignore_files unless you know what you are doing :D
 ignore_files = ['ugit.py','ugit_config.py','urepl.py','urepl_config.py']
