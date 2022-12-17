89d4efbcd63e5d342f89b9b98b905aa09def310d
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2022-12-17 12:39:37 -0600

    Update ugit.py
    Fixed ignore file check

--- ugit.py
+++ ugit.py
@@ -29,8 +29,7 @@ call_trees_url = f'https://api.github.com/repos/{user}/{repository}/git/trees/ma
 raw = f'https://raw.githubusercontent.com/{user}/{repository}/master/'
 
 def pull(f_path,raw_url):
-  print('pulls a single file from github')
-  print('use like ugit.pull(file_path)')
+  print(f'pulling {f_path} from github')
   #files = os.listdir()
   r = urequests.get(raw_url)
   try:
@@ -44,33 +43,42 @@ def pull(f_path,raw_url):
     except:
       print('tried to close new_file to save memory durring raw file decode')
   
-def pull_all_files(tree=call_trees_url,raw = raw):
+def pull_all_files(tree=call_trees_url,raw = raw,ignore = ignore):
   os.chdir('/')
   tree = pull_git_tree()
-  check = []
+  internal_tree = build_internal_tree()
+  internal_tree = remove_ignore(internal_tree)
+  print(' ignore removed ----------------------')
+  print(internal_tree)
+  log = []
   # download and save all files
   for i in tree['tree']:
     if i['type'] == 'tree':
       try:
         os.mkdir(i['path'])
       except:
-        print('failed to make directory may already exist')
-    elif i['path'] not in ugit_config.ignore_files:
+        print(f'failed to {i["path"]} dir may already exist')
+    elif i['path'] not in ignore:
       try:
         os.remove(i['path'])
+        log.append(f'{i["path"]} file removed from int mem')
+        internal_tree = remove_item(i['path'],internal_tree)
       except:
+        log.append(f'{i["path"]} del failed from int mem')
         print('failed to delete old file')
       try:
         pull(i['path'],raw + i['path'])
-        check.append(i['path'] + ' updated')
+        log.append(i['path'] + ' updated')
       except:
-        check.append(i['path'] + ' failed to pull')
+        log.append(i['path'] + ' failed to pull')
   # delete files not in Github tree
-  # Needs work :(
+  if len(internal_tree) > 0:
+      print(internal_tree, ' leftover!')
   logfile = open('ugit_log.py','w')
-  logfile.write(str(check))
+  logfile.write(str(log))
   logfile.close()
   time.sleep(10)
+  print('resetting machine in 10: machine.reset()')
   machine.reset()
   #return check instead return with global
 
@@ -103,7 +111,6 @@ def add_to_tree(dir_item):
       print(f'{dir_item} could not be added to tree')
 
 
-  
 def get_hash(file):
   print(file)
   o_file = open(file)
@@ -137,4 +144,31 @@ def parse_git_tree():
   print('dirs:',dirs)
   print('files:',files)
    
-
+   
+def check_ignore(tree=call_trees_url,raw = raw,ignore = ignore):
+  os.chdir('/')
+  tree = pull_git_tree()
+  check = []
+  # download and save all files
+  for i in tree['tree']:
+    if i['path'] not in ignore:
+        print(i['path'] + ' not in ignore')
+    if i['path'] in ignore:
+        print(i['path']+ ' is in ignore')
+        
+def remove_ignore(internal_tree,ignore=ignore):
+    clean_tree = []
+    int_tree = []
+    for i in internal_tree:
+        int_tree.append(i[0])
+    for i in int_tree:
+        if i not in ignore:
+            clean_tree.append(i)
+    return(clean_tree)
+        
+def remove_item(item,tree):
+    culled = []
+    for i in tree:
+        if item not in i:
+            culled.append(i)
+    return(culled)
