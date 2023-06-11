084506f35b19607a5a56e7eee49f91c7eb66a7e1
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2023-06-11 15:04:31 -0500

    Update ugit.py

--- ugit.py
+++ ugit.py
@@ -32,7 +32,7 @@ default_branch = 'main'
 
 # Don't remove ugit.py from the ignore_files unless you know what you are doing :D
 # Put the files you don't want deleted or updated here use '/filename.ext'
-ignore_files = ['/ugit.py']
+ignore_files = ['/ugit.py','/ugit.backup']
 ignore = ignore_files
 ### -----------END OF USER VARIABLES ----------####
 
